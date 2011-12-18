fa9da80d075d7fc795cd4ece156fab0917e1bfe1
Adam Bair <adambair@gmail.com>
2011-12-18 11:38:31 -0500

    Viewport. Ripped. x boundaries

--- assets/javascripts/application.coffee
+++ assets/javascripts/application.coffee
@@ -1,4 +1,2 @@
-#= require vendor/jquery
-#= require vendor/underscore
 #= require vendor/jaws
 
--- assets/javascripts/game.js
+++ assets/javascripts/game.js
@@ -1,64 +1,105 @@
-var player
-var canvas
-var fps
+function Example() {
+  var player
+  var blocks
+  var fps
+  var viewport
+  var tile_map
 
+  /* Called once when a game state is activated. Use it for one-time setup code. */
+  this.setup = function() {
+    var block_image = "images/block.png"
+    live_info = document.getElementById("live_info")
+    blocks = new jaws.SpriteList()
 
-function setup() {
-  fps     = document.getElementById("fps")
-  canvas  = document.getElementsByTagName('canvas')[0]
-  setup_player()
+    var world = new jaws.Rect(0,0,320*2,320*2)
+    
+    for(var x = 0; x < world.width; x += 16 ) {
+      y = world.height-16
+      blocks.push( new Sprite({image: block_image, x: x, y: y}) )
+    }
 
-  jaws.on_keydown("esc", setup)
-  jaws.preventDefaultKeys(["up", "down", "left", "right", "space"])
+    // Later on we can look them up really fast (see player.move)
+    tile_map = new jaws.TileMap({size: [200,200], cell_size: [16,16]})
+    tile_map.push(blocks)
+
+    viewport = new jaws.Viewport({max_x: world.width, max_y: world.height})
+    player = new jaws.Sprite({x:110, y:320, scale: 2, anchor: "center_bottom"})
+
+    player.move = function() {
+      if(this.x < 15){this.x = 15}
+      if(this.x > world.width-15){this.x = world.width-15}
+      this.x += this.vx
+      if(tile_map.atRect(player.rect()).length > 0) { this.x -= this.vx }
+      this.vx = 0
+
+      this.y += this.vy
+      var block = tile_map.atRect(player.rect())[0]
+      if(block) { 
+        // Heading downwards
+        if(this.vy > 0) { 
+          this.can_jump = true 
+          this.y = block.rect().y - 1
+        }
+        // Heading upwards (jumping)
+        else if(this.vy < 0) {
+          this.y = block.rect().bottom + this.height
+        }
+        this.vy = 0
+      }
+    }
+
+    var anim = new jaws.Animation({sprite_sheet: "images/droid_11x15.png", frame_size: [11,15], frame_duration: 100})
+    player.anim_default = anim.slice(0,5)
+    player.anim_up = anim.slice(6,8)
+    player.anim_down = anim.slice(8,10)
+    player.anim_left = anim.slice(10,12)
+    player.anim_right = anim.slice(12,14)
+    player.vx = player.vy = 0
+    player.can_jump = true
+
+    player.setImage( player.anim_default.next() )
+    // jaws.context.mozImageSmoothingEnabled = false;  // non-blurry, blocky retro scaling
+    jaws.preventDefaultKeys(["up", "down", "left", "right", "space"])
+  }
+
+  /* update() will get called each game tick with your specified FPS. Put game logic here. */
+  this.update = function() {
+    player.setImage( player.anim_default.next() )
+
+    player.vx = 0
+    if(jaws.pressed("left"))  { player.vx = -2; player.setImage(player.anim_left.next()) }
+    if(jaws.pressed("right")) { player.vx = 2;  player.setImage(player.anim_right.next()) }
+    if(jaws.pressed("up"))    { if(player.can_jump) { player.vy = -9; player.can_jump = false } }
+
+    // some gravity
+    player.vy += 0.4
+
+    // apply vx / vy (x velocity / y velocity), check for collision detection in the process.
+    player.move()
+
+    // Tries to center viewport around player.x / player.y.
+    // It won't go outside of 0 or outside of our previously specified max_x, max_y values.
+    viewport.centerAround(player)
+
+    live_info.innerHTML = jaws.game_loop.fps + " fps. Player: " + parseInt(player.x) + "/" + parseInt(player.y) + ". "
+    live_info.innerHTML += "Viewport: " + parseInt(viewport.x) + "/" + parseInt(viewport.y) + "."
+
+  }
+
+  /* Directly after each update draw() will be called. Put all your on-screen operations here. */
+  this.draw = function() {
+    jaws.clear()
+
+    // the viewport magic. wrap all draw()-calls inside viewport.apply and it will draw those relative to the viewport.
+    viewport.apply( function() {
+      blocks.draw()
+      player.draw()
+    });
+  }
 }
 
-function update() {
-  update_player()
-  forceInsideCanvas(player)
+jaws.onload = function() {
+  jaws.unpack()
+  jaws.assets.add(["images/droid_11x15.png","images/block.png"])
+  jaws.start(Example)  // Our convenience function jaws.start() will load assets, call setup and loop update/draw in 60 FPS
 }
-
-function draw() {
-  jaws.clear()
-  player.draw()
-  draw_fps()
-}
-
-function draw_fps() {
-  fps.innerHTML = jaws.game_loop.fps
-}
-
-function isOutsideCanvas(item) { 
-  return (item.x < 0 || item.y < 0 || item.x > canvas.width || item.y > canvas.height) 
-}
-
-function forceInsideCanvas(item) {
-  if(item.x < 0)                    { item.x = 0  }
-  if(item.right > canvas.width)     { item.x = canvas.width - item.width }
-  if(item.y < 0)                    { item.y = 0 }
-  if(item.bottom  > canvas.height)  { item.y = canvas.height - item.height }
-}
-
-function setup_player() {
-  start_x = 10
-  start_y = 10
-  player = new jaws.Sprite({x:start_x, y:start_y, scale: 1, anchor: "center"})
-  
-  var anim = new jaws.Animation({sprite_sheet: "images/player_sprite.png", orientation: 'right', frame_size: [20,20], frame_duration: 100})
-  player.anim_default = anim.slice(0,1)
-  player.anim_up = anim.slice(1,2)
-  player.anim_right = anim.slice(2,3)
-  player.anim_down = anim.slice(3,4)
-  player.anim_left = anim.slice(4,5)
-  player.setImage( player.anim_default.next() )
-}
-
-function update_player() {
-  player.setImage( player.anim_default.next() )
-  if(jaws.pressed("left"))  { player.x -= 2; player.setImage(player.anim_left.next())}
-  if(jaws.pressed("right")) { player.x += 2; player.setImage(player.anim_right.next())}
-  if(jaws.pressed("up"))    { player.y -= 2; player.setImage(player.anim_up.next()) }
-  if(jaws.pressed("down"))  { player.y += 2; player.setImage(player.anim_down.next()) }
-}
-
-jaws.assets.add("images/player_sprite.png")
-jaws.start()
